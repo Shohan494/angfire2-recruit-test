64b995641dfd596fee56e6470fc251811d8c0cf6
shohan494 <shohan12224943@gmail.com>
2017-02-21 23:02:30 +0600

    firebase angular 2 project initialized with dependencies and configured firebase in 'src/app/app.module.ts'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+
 
 import { AppComponent } from './app.component';
+
+  // Initialize Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyAw-SU_Y-Vjx-IgAkF4hGq5sB-dGSYFeSU",
+  authDomain: "ang2form.firebaseapp.com",
+  databaseURL: "https://ang2form.firebaseio.com",
+  storageBucket: "ang2form.appspot.com",
+  messagingSenderId: "905651257461"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
